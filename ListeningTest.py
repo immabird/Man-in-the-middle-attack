50a099b7918255edeffb72c6b29a4ff09fbc4483
Andrew Mellen <mellena1@wit.edu>
2017-04-09 21:29:51 -0400

    new listening

--- ListeningTest.py
+++ ListeningTest.py
@@ -1,34 +1,13 @@
-#!/usr/bin/env python
-import struct
 import socket
-import binascii
 
-rawSocket=socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.htons(0x0800))
-#ifconfig eth0 promisc up
-rawSocket.bind(('',443))
-receivedPacket=rawSocket.recvfrom(2048)
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
-#Ethernet Header...
-ethernetHeader=receivedPacket[0:14]
-ethrheader=struct.unpack("!6s6s2s",ethernetHeader)
-destinationIP= binascii.hexlify(ethrheader[0])
-sourceIP= binascii.hexlify(ethrheader[1])
-protocol= binascii.hexlify(ethrheader[2])
-print("Destinatiom: " + destinationIP)
-print("Souce: " + sourceIP)
-print("Protocol: "+ protocol)
+while True:
+    packet = s.recvfrom(65565)
+    packet = packet[0]
+    ip_header = packet[0:20]
+    iph = unpack('!BBHHHBBH4s4s', ip_header)
 
-#IP Header...
-ipHeader=receivedPacket[0][14:34]
-ipHdr=struct.unpack("!12s4s4s",ipHeader)
-destinationIP=socket.inet_ntoa(ipHdr[2])
-print("Source IP: " +sourceIP)
-print("Destination IP: "+destinationIP)
-
-#TCP Header...
-tcpHeader=receivedPacket[0][34:54]
-tcpHdr=struct.unpack("!2s2s16s",tcpHeader)
-sourcePort=socket.inet_ntoa(tcpHdr[0])
-destinationPort=socket.inet_ntoa(tcpHdr[1])
-print("Source Port: " + sourcePort)
-print("Destination Port: " + destinationPort)
+    s_addr = socket.inet_ntoa(iph[8])
+    if s_addr != '127.0.0.1':
+        print(s_addr)
